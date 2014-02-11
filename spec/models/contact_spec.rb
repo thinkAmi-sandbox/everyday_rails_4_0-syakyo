c253208f2088f64cd4c0c6644ed9e32532a84ce0
thinkAmi <dev.thinkami@gmail.com>
2014-02-11 07:29:23 +0000

    p21-2

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -56,4 +56,19 @@ describe Contact do
 
     expect(Contact.by_letter("J")).to eq [johnson, jones]
   end
+
+  it "returns a sorted array of results that match" do
+    smith = Contact.create(firstname: 'John',
+                           lastname:  'Smith',
+                           email:     'jsmith@example.com')
+    jones = Contact.create(firstname: 'Tim',
+                           lastname:  'Jones',
+                           email:     'tjones@example.com')
+    johnson = Contact.create(
+                           firstname: 'John',
+                           lastname:  'Johnson',
+                           email:     'jjohnson@example.com')
+
+    expect(Contact.by_letter("J")).not_to include smith
+  end
 end
