522256fde0c6481e615de83102ecd5dc0c4e00e2
thinkAmi <dev.thinkami@gmail.com>
2014-02-11 06:24:47 +0000

    p16

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 describe Contact do
 
-  it "is valid with a firstname, lastname and email"
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+
 
   it "is invalid without a firstname"
 
