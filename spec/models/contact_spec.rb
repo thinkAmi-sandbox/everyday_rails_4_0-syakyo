d7250ad1935c695130f3d59bd52b138748b61bd7
thinkAmi <dev.thinkami@gmail.com>
2014-02-17 21:20:28 +0000

    p33

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Contact do
   it "has a valid factory" do
-    expect(FactoryGirl.build(:contact)).to be_valid
+    expect(create(:contact)).to be_valid
   end
 
 
@@ -16,35 +16,35 @@ describe Contact do
 
 
   it "is invalid without a firstname" do
-    contact = FactoryGirl.build(:contact, firstname: nil)
+    contact = build(:contact, firstname: nil)
     expect(contact).to have(1).errors_on(:firstname)
   end
 
 
   it "is invalid without a lastname" do
-    contact = FactoryGirl.build(:contact, lastname: nil)
+    contact = build(:contact, lastname: nil)
     expect(contact).to have(1).errors_on(:lastname)
   end
 
 
   it "is invalid without an email address" do
-    contact = FactoryGirl.build(:contact, email: nil)
+    contact = build(:contact, email: nil)
     expect(contact).to have(1).errors_on(:email)
   end
 
 
   it "is invalid with a duplicate email address" do
-    FactoryGirl.create(:contact, email: "aaron@example.com")
-    contact = FactoryGirl.build(:contact, email: "aaron@example.com")
+    create(:contact, email: "aaron@example.com")
+    contact = build(:contact, email: "aaron@example.com")
 
     expect(contact).to have(1).errors_on(:email)
   end
 
 
   it "returns a contact's full name as a string" do
-    contact = FactoryGirl.build(:contact,
-                                firstname: 'John',
-                                lastname:  'Doe')
+    contact = build(:contact,
+                    firstname: 'John',
+                    lastname:  'Doe')
 
     expect(contact.name).to eq 'John Doe'
   end
