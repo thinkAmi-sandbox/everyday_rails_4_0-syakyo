08bc76e644172fd478c581d126b6dc674e7378b0
thinkAmi <dev.thinkami@gmail.com>
2014-02-18 21:28:39 +0000

    p39

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,49 +2,40 @@ require 'spec_helper'
 
 describe Contact do
   it "has a valid factory" do
-    expect(create(:contact)).to be_valid
-  end
-
-
-  it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname:  'Sumner',
-      email:     'tester@example.com')
-    expect(contact).to be_valid
+    expect(FactoryGirl.create(:contact)).to be_valid
   end
 
 
   it "is invalid without a firstname" do
-    contact = build(:contact, firstname: nil)
+    contact = FactoryGirl.build(:contact, firstname: nil)
     expect(contact).to have(1).errors_on(:firstname)
   end
 
 
   it "is invalid without a lastname" do
-    contact = build(:contact, lastname: nil)
+    contact = FactoryGirl.build(:contact, lastname: nil)
     expect(contact).to have(1).errors_on(:lastname)
   end
 
 
   it "is invalid without an email address" do
-    contact = build(:contact, email: nil)
+    contact = FactoryGirl.build(:contact, email: nil)
     expect(contact).to have(1).errors_on(:email)
   end
 
 
   it "is invalid with a duplicate email address" do
-    create(:contact, email: "aaron@example.com")
-    contact = build(:contact, email: "aaron@example.com")
+    FactoryGirl.create(:contact, email: "aaron@example.com")
+    contact = FactoryGirl.build(:contact, email: "aaron@example.com")
 
     expect(contact).to have(1).errors_on(:email)
   end
 
 
   it "returns a contact's full name as a string" do
-    contact = build(:contact,
-                    firstname: 'John',
-                    lastname:  'Doe')
+    contact = FactoryGirl.build(:contact,
+                                firstname: 'John',
+                                lastname:  'Doe')
 
     expect(contact.name).to eq 'John Doe'
   end
@@ -52,14 +43,14 @@ describe Contact do
 
   describe "fileter last name by letter" do
     before :each do
-      @smith = Contact.create(firstname: 'John',
+      @smith = FactoryGirl.create(:contact,
                               lastname:  'Smith',
                               email:     'jsmith@example.com')
-      @jones = Contact.create(firstname: 'Tim',
+      @jones = FactoryGirl.create(:contact,
                               lastname:  'Jones',
                               email:     'tjones@example.com')
-      @johnson = Contact.create(
-                              firstname: 'John',
+      @johnson = FactoryGirl.create(
+                              :contact,
                               lastname:  'Johnson',
                               email:     'jjohnson@example.com')
     end
@@ -80,6 +71,6 @@ describe Contact do
 
 
   it "has three phone numbers" do
-    expect(create(:contact).phones.count).to eq 3
+    expect(FactoryGirl.create(:contact).phones.count).to eq 3
   end
 end
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,21 +2,20 @@ require 'spec_helper'
 
 describe Phone do
   it "does not allow duplicate phone numbers per contact" do
-    contact = create(:contact)
-    home_phone = create(:home_phone,
-                        contact: contact,
-                        phone:   '785-555-1234')
-    mobile_phone = build(:mobile_phone,
-                         contact: contact,
-                         phone: '785-555-1234')
+    contact = FactoryGirl.create(:contact)
+    home_phone = FactoryGirl.create(:home_phone,
+                                    contact: contact,
+                                    phone:   '785-555-1234')
+    mobile_phone = FactoryGirl.build(:mobile_phone,
+                                     contact: contact,
+                                     phone: '785-555-1234')
 
     expect(mobile_phone).to have(1).errors_on(:phone)
   end
 
 
   it "allows two contacts to share a phone number" do
-    create(:home_phone,
-           phone: "785-555-1234")
-    expect(build(:home_phone, phone: "785-555-1234")).to be_valid
+    FactoryGirl.create(:home_phone, phone: "785-555-1234")
+    expect(FactoryGirl.build(:home_phone, phone: "785-555-1234")).to be_valid
   end
 end
