4f58194723165e390237118033bc9fad1be0fe3b
thinkAmi <dev.thinkami@gmail.com>
2014-02-22 22:11:00 +0000

    p101-2

--- spec/features/news_releases_spec.rb
+++ spec/features/news_releases_spec.rb
@@ -2,7 +2,24 @@ require 'spec_helper'
 
 feature "News releases" do
   context "as a user" do
-    scenario "adds a news release"
+    scenario "adds a news release" do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      visit root_path
+      click_link "News"
+
+      expect(page).not_to have_content "BigCo switches to Rails"
+      click_link "Add News Release"
+
+      fill_in "Date",  with: "2013-07-29"
+      fill_in "Title", with: "BigCo siwtches to Rails"
+      fill_in "Body",  with: "BigCo has released a new website built with open source."
+      click_button "Create News release"
+
+      expect(current_path).to eq news_release_path
+      expect(page).to have_content "Successfully created news release."
+      expect(page).to have_content "2013-07-29: BigCo siwtches to Rails"
+    end
   end
 
   context "as a guest" do
