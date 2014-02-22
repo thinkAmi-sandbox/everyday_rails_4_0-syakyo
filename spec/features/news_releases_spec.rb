b5a7a0478bf70dd20527cd913051b11fd723f89d
thinkAmi <dev.thinkami@gmail.com>
2014-02-22 22:04:59 +0000

    p101-1

---
+++ spec/features/news_releases_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature "News releases" do
+  context "as a user" do
+    scenario "adds a news release"
+  end
+
+  context "as a guest" do
+    scenario "reads a news release"
+  end
+end
